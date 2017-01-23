5e197cf15c6b716814ddf3e2272fe53285985167
William Wong <compulim@hotmail.com>
2017-01-23 14:49:52 +0800

    Use Promise to simplify response

--- lambda/index.js
+++ lambda/index.js
@@ -1,145 +1,184 @@
 'use strict';
 
-const AZURE_IOTHUB_CONNECTION_STRING         = 'HostName=codeaholics-iothub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=ftHSBjm+qdxO0+XwpzsRVkjp9quzZeXNTVfZjezGP5M=';
-const AZURE_IOTHUB_TIMEOUT_IN_SECONDS        = 5;
-const AZURE_IOTHUB_DEVICE_REGISTRY_DEVICE_ID = 'deviceRegistry';
+const AZURE_IOTHUB_CONNECTION_STRING  = 'HostName=codeaholics-iothub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=ftHSBjm+qdxO0+XwpzsRVkjp9quzZeXNTVfZjezGP5M=';
+const AZURE_IOTHUB_TIMEOUT_IN_SECONDS = 5;
+const AZURE_IOTHUB_BRIDGE_DEVICE_ID   = 'bridge';
 
 const Client  = require('azure-iothub').Client;
 const debug   = require('debug')('lambda');
 const Message = require('azure-iot-common').Message;
+const uuidV4  = require('uuid/v4');
 
 exports.handler = function (event, context) {
   debug('handler', `received a request ${ JSON.stringify(event) }`);
 
+  let promise;
+
   switch (event.header.namespace) {
   case 'Alexa.ConnectedHome.Discovery':
-    handleDiscovery(event, context);
+    promise = handleDiscovery(event);
     break;
 
   case 'Alexa.ConnectedHome.Control':
-    handleControl(event, context);
+    promise = handleControl(event);
     break;
 
   case 'Alexa.ConnectedHome.System':
-    handleSystem(event, context);
+    promise = handleSystem(event);
     break;
-
-  default:
-    return event.fail(generateError(
-      event,
-      new Error('not supported'),
-      'UnsupportedOperationError'
-    ));
   }
+
+  if (!promise) {
+    const notSupportedError = new Error('not supported');
+
+    notSupportedError.alexaHeader = {
+      namespace     : event.header.namespace,
+      name          : 'UnsupportedOperationError',
+      payloadVersion: '2'
+    };
+
+    promise = Promise.reject(notSupportedError);
+  }
+
+  promise.then(
+    res => {
+      context.succeed({
+        header: Object.assign({
+          messageId     : uuidV4(),
+          namespace     : event.header.namespace,
+          name          : generateResponseName(event.header.name),
+          payloadVersion: '2'
+        }, res.header),
+        payload: res.payload
+      });
+    },
+    err => {
+      context.fail({
+        header: Object.assign({
+          messageId     : uuidV4(),
+          namespace     : event.header.namespace,
+          name          : 'DriverInternalError',
+          payloadVersion: '2'
+        }, err.alexaHeader),
+        payload: {
+          code        : (err.response && err.response.headers) ? err.response.headers['iothub-errorcode'] : (err.code || 'UNKNOWN'),
+          description : err.message,
+          responseBody: err.responseBody,
+          stack       : err.stack,
+          statusCode  : err.response && err.response.statusCode
+        }
+      });
+    }
+  )
 };
 
-function handleDiscovery(event, context) {
+function handleDiscovery(event) {
   switch (event.header.name) {
   case 'DiscoverAppliancesRequest':
-    return handleDiscoverAppliances(event, context);
-
-  default:
-    return event.fail(generateError(
-      event,
-      new Error('not supported'),
-      'UnsupportedOperationError'
-    ));
+    return handleDiscoverAppliances(event.payload);
   }
 }
 
-function handleDiscoverAppliances(event, context) {
+function handleDiscoverAppliances() {
   debug('discovery', 'handling discovery appliances request');
 
-  invokeDeviceMethod(event, context, AZURE_IOTHUB_DEVICE_REGISTRY_DEVICE_ID, 'discover', {});
+  return invokeDeviceMethod(AZURE_IOTHUB_BRIDGE_DEVICE_ID, 'discover', { source: 'alexa' }, 'DiscoverAppliancesResponse');
 }
 
-function handleControl(event, context) {
+function handleControl(event) {
   switch (event.header.name) {
   case 'TurnOnRequest':
-    handleTurnOnRequest(event, context);
-    break;
+    return handleTurnOnRequest(event.payload);
 
   case 'TurnOffRequest':
-    handleTurnOffRequest(event, context);
-    break;
-
-  default:
-    context.fail(
-      generateError(
-        event,
-        new Error('not supported'),
-        'UnsupportedOperationError'
-      )
-    );
-
-    break;
+    return handleTurnOffRequest(event.payload);
   }
 }
 
-function handleTurnOnRequest(event, context) {
-  invokeDeviceMethod(event, context, event.payload.appliance.applianceId, 'turnOn', { source: 'alexa' }, 'TurnOnConfirmation');
+function handleTurnOnRequest(payload) {
+  return invokeDeviceMethod(payload.appliance.applianceId, 'turnOn', { source: 'alexa' }, 'TurnOnConfirmation');
 }
 
-function handleTurnOffRequest(event, context) {
-  invokeDeviceMethod(event, context, event.payload.appliance.applianceId, 'turnOff', { source: 'alexa' }, 'TurnOffConfirmation');
+function handleTurnOffRequest(payload) {
+  return invokeDeviceMethod(payload.appliance.applianceId, 'turnOff', { source: 'alexa' }, 'TurnOffConfirmation');
 }
 
-function handleSystem(event, context) {
+function handleSystem(event) {
   switch (event.header.name) {
   case 'HealthCheckRequest':
-    handleHealthCheckRequest(event, context);
-    break;
-
-  default:
-    context.fail(
-      generateError(
-        event,
-        new Error('not supported'),
-        'UnsupportedOperationError'
-      )
-    );
-
-    break;
+    return handleHealthCheckRequest(event.payload);
   }
 }
 
-function handleHealthCheckRequest(event, context) {
-  invokeDeviceMethod(event, context, AZURE_IOTHUB_DEVICE_REGISTRY_DEVICE_ID, 'healthCheck', { description: 'The system is currently healthy', isHealthy: true }, 'HealthCheckResponse');
+function handleHealthCheckRequest(payload) {
+  return invokeDeviceMethod(AZURE_IOTHUB_BRIDGE_DEVICE_ID, 'healthCheck', {})
+    .then(
+      () => ({
+        header: {
+          name: 'HealthCheckResponse'
+        },
+        payload: {
+          description: 'The system is currently healthy',
+          isHealthy: true
+        }
+      }),
+      err => ({
+        header: {
+          name: 'HealthCheckResponse'
+        },
+        payload: {
+          description: 'The system is currently down',
+          isHealthy: false
+        }
+      })
+    );
 }
 
-function invokeDeviceMethod(event, context, deviceID, methodName, payload, responseName) {
-  executeOnAzure(
-    function (client, callback) {
-      client.invokeDeviceMethod(
-        deviceID,
-        {
-          methodName              : methodName,
-          payload                 : payload,
-          responseTimeoutInSeconds: AZURE_IOTHUB_TIMEOUT_IN_SECONDS
-        },
-        callback
-      );
-    },
-    function (err, result) {
-      if (!err && result.status === 200) {
-        context.succeed(generateResponse(event, result.payload, responseName));
-      } else {
-        switch (err && err.response && err.response.headers['iothub-errorcode']) {
-        case 'DeviceNotOnline':
-          context.fail(generateError(event, err, 'TargetOfflineError'));
-          break;
+function invokeDeviceMethod(deviceID, methodName, payload, responseName) {
+  return new Promise((resolve, reject) => {
+    executeOnAzure(
+      (client, callback) => {
+        client.invokeDeviceMethod(
+          deviceID,
+          {
+            methodName              : methodName,
+            payload                 : payload,
+            responseTimeoutInSeconds: AZURE_IOTHUB_TIMEOUT_IN_SECONDS
+          },
+          callback
+        );
+      },
+      (err, result) => {
+        if (!err && result.status === 200) {
+          resolve({
+            header: {
+              name: responseName
+            },
+            payload: result.payload
+          });
+        } else if (err) {
+          switch (err.response && err.response.headers['iothub-errorcode']) {
+          case 'DeviceNotOnline':
+            err.alexaHeader = { name: 'TargetOfflineError' };
+            break;
 
-        case 'DeviceNotFound':
-          context.fail(generateError(event, err, 'NoSuchTargetError'));
-          break;
+          case 'DeviceNotFound':
+            err.alexaHeader = { name: 'NoSuchTargetError' };
+            break;
+          }
 
-        default:
-          context.fail(generateError(event, err, 'DriverInternalError'));
-          break;
+          reject(err);
+        } else {
+          const error = new Error(`server returned ${ result.status }`);
+
+          error.message = result.payload.message;
+          error.stack = result.payload.stack;
+
+          reject(error);
         }
       }
-    }
-  );
+    );
+  });
 }
 
 function executeOnAzure(executor, callback) {
@@ -170,36 +209,6 @@ function executeOnAzure(executor, callback) {
       });
     });
   });
-}
-
-function generateResponse(event, payload, responseName) {
-  return {
-    header: {
-      messageId     : event.header.messageId,
-      namespace     : event.header.namespace,
-      name          : responseName || generateResponseName(event.header.name),
-      payloadVersion: event.header.payloadVersion
-    },
-    payload: payload
-  };
-}
-
-function generateError(event, err, responseName) {
-  return generateResponse(
-    event,
-    err ?
-      {
-        exception: {
-          code        : (err.response && err.response.headers) ? err.response.headers['iothub-errorcode'] : (err.code || 'UNKNOWN'),
-          description : err.message,
-          responseBody: err.responseBody,
-          statusCode  : err.response && err.response.statusCode
-        }
-      }
-    :
-      {},
-    responseName
-  );
 }
 
 function generateResponseName(name) {
